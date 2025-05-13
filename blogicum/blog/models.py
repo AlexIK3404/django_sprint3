468eae6613f966de83ad28a7ef4b89687017a2fa
Александр Коняев <alexkonyaev6@gmail.com>
2025-05-13 17:26:13 +0300

    Fourth commit

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -25,8 +25,8 @@ class Category(BaseModel):
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
-        help_text='Идентификатор страницы для URL; разрешены символы\
-        латиницы, цифры, дефис и подчёркивание.'
+        help_text='Идентификатор страницы для URL; '
+        'разрешены символы латиницы, цифры, дефис и подчёркивание.'
     )
 
     def __str__(self):
@@ -53,8 +53,8 @@ class Post(BaseModel):
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text='Если установить дату и время в будущем — можно\
-        делать отложенные публикации.'
+        help_text='Если установить дату и время в будущем — '
+        'можно делать отложенные публикации.'
     )
     author = models.ForeignKey(
         User,
